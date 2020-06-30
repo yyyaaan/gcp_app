791a549d8239049c769b7ffb84ccf8f2cad31ae8
Yan @ GCP <yan@yan.fi>
2020-06-30 11:30:42 +0100

    1st deployment

--- main.py
+++ main.py
@@ -7,27 +7,34 @@ from google.cloud import bigquery
 
 app = flask.Flask(__name__)
 bigquery_client = bigquery.Client()
-q_from = "Sydney"
-q_to = "Helsinki"
-q_ddate = "2021-01-01"
+QUERY =  """
+  select distinct route, concat(`from`, '>', `to`, ' on ',  FORMAT_DATE("%d%b", ddate)) as flight, ceiling(eur) as eur, cast(tss as DATE) as tss
+  from `yyyaaannn.Explore.QR01`
+  where (`from` = '{}' and `to` = '{}' and `ddate` = DATE('{}'))
+     or (`from` = '{}' and `to` = '{}' and `ddate` = DATE('{}'))
+"""
 
 
 @app.route("/")
 def main():
 
-    QUERY =  """
-        select distinct route, ddate, ceiling(eur) as eurc, eur, cast(tss as DATE) as tss
-        from `yyyaaannn.Explore.QR01`
-        where `from` = '{}' and `to` = '{}' and `ddate` = DATE('{}') 
-    """
+    q_route = flask.request.args.get("q_route")
+    q_ddate = flask.request.args.get("q_ddate")
+    q_rdate = flask.request.args.get("q_rdate")
 
-    QUERY = QUERY.format(q_from, q_to, q_ddate)
+    # override if empty
+    if q_ddate is None: q_ddate = "2021-01-01"
+    if q_rdate is None: q_rdate = "2021-01-06"
+    if q_route is None: q_route = "Helsinki Canberra|Sydney Oslo"
 
-    query_job = bigquery_client.query(QUERY)
+    q_dests = q_route.replace("|", " ").split(" ")
+
+    query_job = bigquery_client.query(QUERY.format(q_dests[0], q_dests[1], q_ddate, q_dests[2], q_dests[3], q_rdate))
 
     return flask.redirect(
         flask.url_for(
             "results",
+            text=q_route + q_ddate + q_rdate,
             project_id=query_job.project,
             job_id=query_job.job_id,
             location=query_job.location,
@@ -41,27 +48,18 @@ def results():
     job_id = flask.request.args.get("job_id")
     location = flask.request.args.get("location")
 
-    query_job = bigquery_client.get_job(
-        job_id,
-        project=project_id,
-        location=location,
-    )
+    query_job = bigquery_client.get_job(job_id, project=project_id, location=location,)
 
     try:
         df = query_job.result(timeout=20).to_dataframe()
     except concurrent.futures.TimeoutError:
         return flask.render_template("timeout.html", job_id=query_job.job_id)
 
-    fig = px.bar(df, x='tss', y='eur', color='route', barmode='group')
-    fig.update_layout(
-        title="{} -> {} on {}<br>hover for details|double click to filter routes".format(q_from, q_to, q_ddate),
-        xaxis_title=None,
-        yaxis_title="EUR"
-    )
+    fig = px.bar(df, x='tss', y='eur', color='route', barmode='group', facet_row='flight', height=900)
     
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return flask.render_template("result.html", plot=graphJSON)
+    return flask.render_template("result.html", plot=graphJSON, text= flask.request.args.get("text"))
 
 
 if __name__ == "__main__":
